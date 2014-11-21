cb8b4f56976a304248a91935efcb25055b0e599b
Alan Eicker <alan.eicker@realpage.com>
2014-11-21 13:52:41 -0600

    orientation change bug fix

--- scripts/directives.js
+++ scripts/directives.js
@@ -5,6 +5,12 @@
     directives.directive('globalDirectives', function ($window, $document) {
         return function (scope, element, attributes) {
 
+            var w = angular.element($window);
+
+            w.on('orientationchange', function () {
+                w.scrollTop(0);
+            });
+
             scope.getProfileImg = function () {
                 var profileImgsSrc = $('#profile-avatar').attr('src');
                 return profileImgsSrc.indexOf('data:image') !== -1 ? profileImgsSrc : '/images/default_profile_img.png';
